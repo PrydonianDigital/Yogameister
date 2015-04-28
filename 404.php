480202a25e8c2828a183065574febe964a26ff4f
vbloke <vbloke@gmail.com>
2015-04-28 17:37:25 +0100

    404 page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div class="row" id="page">
+
+		<div class="twelve columns">
+
+		<h2>Page not found</h2>
+
+		<p>We've had a bit of work done and this page has now moved. Please use the menu above to go to another page.</p>
+
+	</div>
+
+</div>
+
+<?php get_footer(); ?>
